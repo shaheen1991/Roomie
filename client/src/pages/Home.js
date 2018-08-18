f728c820ef856293be8c0a83053c9b93e0ee2e65
Riya Abraham <riyamaryabraham12@gmail.com>
2018-08-18 11:07:12 -0500

    got landing page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
-import Landing from "../components/Navbar";
+import Landing from "../components/Landing";
 
 const Home = () => (
   <Landing />
