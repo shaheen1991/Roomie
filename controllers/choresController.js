b86a99def35f42d85fe2c2f7ae5ce8eab116db45
Riya Abraham <riyamaryabraham12@gmail.com>
2018-08-25 13:24:01 -0500

    simple change

--- controllers/choresController.js
+++ controllers/choresController.js
@@ -3,9 +3,10 @@ const db = require("../models");
 // Defining methods for the choresController
 module.exports = {
   findAll: function(req, res) {
+    console.log("in db chore");
     db.Chore
       .find(req.query)
-      .sort({ date: -1 })
+     
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
