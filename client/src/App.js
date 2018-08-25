148abe71755e38fc6c66b41a9755aa06af5e62df
Shaheen <36688626+shaheen1991@users.noreply.github.com>
2018-08-25 14:04:25 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,48 @@
+// // import React from "react";
+// // import { BrowserRouter as Router, Route } from "react-router-dom";
+// import About from "./pages/About";
+// import Comments from "./pages/Comments";
+// import Home from "./pages/Home";
+// import Login from "./pages/Login";
+// // import Navbar from "./components/Navbar";
+// import Footer from "./components/Footer";
+// import Wrapper from "./components/Wrapper";
+import './css/App.css';
+
+///////////////////////
 import axios from 'axios';
 import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
 import Navbar from './components/Navbar/Navbar';
+
 import { withUser, update } from './services/withUser';
+
 import CreateAccountPage from './pages/CreateAccountPage';
+// import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import NotFoundPage from './pages/NotFoundPage';
-import SmackTalk from "./pages/SmackTalk";
+// import SmackTalk from "./pages/SmackTalk";
 import ChoreForm from "./pages/ChoreForm";
+// import SmackTalk from './pages/SmackTalk';
+import SmackTalk from './pages/Smacktalk';
 import Calendar from './pages/Calendar';
 import Home from './pages/Home';
-import './css/App.css';
 
 class App extends Component {
   componentDidMount() {
+    // this is going to double check that the user is still actually logged in
+    // if the app is reloaded. it's possible that we still have a user in sessionStorage
+    // but the user's session cookie expired.
     axios.get('/api/auth')
       .then(res => {
+        // if we get here, the user's session is still good. we'll update the user
+        // to make sure we're using the most recent values just in case
         update(res.data);
       })
       .catch(err => {
+        // if we get a 401 response, that means the user is no longer logged in
         if (err.response.status === 401) {
           update(null);
         }
