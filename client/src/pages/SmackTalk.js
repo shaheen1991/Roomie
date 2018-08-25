c96bd13c986707483fe4a4892e0cd2347aaaaa06
Shaheen <36688626+shaheen1991@users.noreply.github.com>
2018-08-25 14:08:31 -0500

    Update SmackTalk.js

--- client/src/pages/SmackTalk.js
+++ client/src/pages/SmackTalk.js
@@ -1,75 +1,29 @@
-import React, {Component} from "react";
+import React from "react";
 import Container from "../components/Container";
 import Row from "../components/Row";
 import Col from "../components/Col";
 // import Navbar1 from "../components/Navbar";
 import Footer from "../components/Footer";
 import CommentBox from "../components/Chat";
-import LoginButton from '../components/LoginButton';
-import { withUser } from '../services/withUser';
 
+const SmackTalk = () => (
+  <div>
+    <Container style={{ marginTop: 30 }}>
+      <Row>
+        <Col size="md-12">
+          <h1 style={{fontFamily:"'Alegreya Sans SC', sans-serif"}}>SmackTalk</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col size="md-12">
 
-
-class SmackTalk extends Component {
-  state={
-    events:[]
-  }
-  render(){
-    const { user } = this.props;
-    const username = user ? user.username : null;
-    const handleLogIn = () => {
-    this.props.history.push('/login');
-    };
-    return(
-      <div>
-    
-          {/* <Navbar1 /> */}
-           {user ?
-           <Container style={{ marginTop: 30 }}>
-             <Row>
-            <Col size="md-12">
-              <h1 style={{fontFamily:"'Alegreya Sans SC', sans-serif"}}>SmackTalk</h1>
-             </Col>
-            </Row>
-           <Row>
-              <Col size="md-12">
-      
-                 <CommentBox/>
-                
-               </Col>
-            </Row>
-           </Container>
-           : <LoginButton onClick={handleLogIn} />}
-          <Footer />
-        </div>
-
-    )
-  }
-};  
-
-// const SmackTalk = () => (
-
-//   <div>
-    
-//     {/* <Navbar1 /> */}
-//     {user ?
-//     <Container style={{ marginTop: 30 }}>
-//       <Row>
-//         <Col size="md-12">
-//           <h1 style={{fontFamily:"'Alegreya Sans SC', sans-serif"}}>SmackTalk</h1>
-//         </Col>
-//       </Row>
-//       <Row>
-//         <Col size="md-12">
-
-//            <CommentBox/>
+           <CommentBox/>
           
-//         </Col>
-//       </Row>
-//     </Container>
-//     : <LoginButton onClick={handleLogIn} />}
-//     <Footer />
-//   </div>
-// );
+        </Col>
+      </Row>
+    </Container>
+    <Footer />
+  </div>
+);
 
-export default withUser(SmackTalk);
+export default SmackTalk;
